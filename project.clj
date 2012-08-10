d828dc6f86e6655e9f125a3f28301be80b0740ca
Alex Osborne <ato@meshy.org>
2012-08-10 22:49:54 +1000

    Release 1.0.1.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.ato/lein-release "1.0.1-SNAPSHOT"
-  :description "Leiningen Release Plugin (ato's fork)"
+(defproject org.clojars.ato/lein-release "1.0.1"
+  :description "Leiningen Release Plugin (ato's fork with concise tags and commit messages)."
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :eval-in :leiningen
-  :lein-release {:deploy-via :clojars}
+  :lein-release {:deploy-via :lein-install}
   :dependencies [[org.clojure/clojure "1.3.0"]])
