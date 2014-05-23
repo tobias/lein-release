2fb95755f822de30ac2122932c38f5efefa6d11b
Toby Crawley <toby@tcrawley.org>
2014-05-23 12:55:20 -0400

    Release 1.0.6.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tcrawley/lein-release "1.0.6-SNAPSHOT"
+(defproject org.clojars.tcrawley/lein-release "1.0.6"
   :description "Leiningen Release Plugin (tcrawley's fork of ato's fork)"
   :url "https://github.com/ato/lein-release"
   :dev-dependencies [[swank-clojure "1.4.2"]]
